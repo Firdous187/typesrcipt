a52a2c7e9a5812ea0149de117619d3340e675f56
Firdous187 <Shaik.firdous0011@gmail.com>
2024-08-14 11:23:54 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hi! Good Morning darling</h1>
+     <h2>My dear best frnd Sravs</h2>
+     <h3>wish u the many more happy return of the day</h3>
+     <img src="./images/IMG1.jpg"></img>
+     <img src="./images/IMG2.jpg"></img>
+    </div>
+  );
+}
+
+export default App;
